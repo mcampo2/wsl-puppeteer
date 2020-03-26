dc25d8f7e945143a1694d3a5785e1092524016f9
Michael Campo <mcampo2@gmail.com>
2020-03-26 07:30:39 -0400

    Initial commit of code

---
+++ app.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+if(require.main === module) {
+  console.log('puppeteer: app.js');
+}
+
+const puppeteer = require('puppeteer');
+var args = { args: ['--no-sandbox', '--single-process'] };
+
+var main = async function() {
+  console.log('Opening Browser...');
+  const browser = await puppeteer.launch(args);
+  console.log('Initializing Window...');
+  const page = await browser.newPage();
+  await page.setJavaScriptEnabled(false);
+  await page.setViewport({ height: 480, width: 640 });
+  console.log('Opening Webpage...');
+  page.goto('https://duckduckgo.com/');
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  console.log('Printing Webpage...');
+  await page.screenshot({ path: 'page.png' });
+  console.log('Closing Browser...');
+  await browser.close();
+};
+
+main();
